1e9afe3259fc77d0dee30dc964e7532d0b05e2bd
Arunika-negi <negiarunika0001@gmail.com>
2025-11-25 17:26:57 +0530

    Add files via upload

---
+++ Leukemia_Backend/API/app.py
@@ -0,0 +1,55 @@
+
+
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from PIL import Image
+import numpy as np
+from io import BytesIO
+import tensorflow as tf
+
+app = FastAPI(title="Leukemia CNN API")
+MODEL = tf.keras.models.load_model("Models/leukemia_cnn_model.keras")
+
+class_names = ["ALL", "AML", "CLL", "CML", "NORMAL"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+IMAGE_SIZE = 224
+@app.get("/ping")
+async def ping():
+    return {"message": "API is running!"}
+def read_image(data):
+    image = Image.open(BytesIO(data)).convert("RGB")
+    image = image.resize((IMAGE_SIZE, IMAGE_SIZE))
+    image = np.array(image) / 255.0
+    image = np.expand_dims(image, axis=0)   # shape = (1, 224, 224, 3)
+    return image
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    data = await file.read()
+    img = read_image(data)
+    prediction = MODEL.predict(img)[0]
+    idx = int(np.argmax(prediction))
+    label = class_names[idx]
+    confidence = float(prediction[idx]) * 100
+
+    return {
+        "prediction": label,
+        "confidence": round(confidence, 2),
+        "probabilities": {
+            "ALL": float(prediction[0]),
+            "AML": float(prediction[1]),
+            "CLL": float(prediction[2]),
+            "CML": float(prediction[3]),
+            "NORMAL": float(prediction[4]),
+        }
+    }
+
+
+
